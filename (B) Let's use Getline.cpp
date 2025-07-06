4bfac06b3664716b56b57784a90907fd66d3fbc6
Alfi Shahrin <arikashahrin04.95@gmail.com>
2025-07-06 20:40:43 +0600

    (B) Let's use Getline.cpp

---
+++ (B) Let's use Getline.cpp
@@ -0,0 +1,20 @@
+1.// Problem name: (B) Let's use Getline.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 23/6/25
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+string s;
+getline(cin,s);
+for(int i=0; i<s.size(); i++)
+{
+if(s[i]== '\\'){
+break;
+}
+else
+cout<<s[i];
+}
+}
